9e4ef3d62a2066133a162baf760b47956eb73bb2
vanphuagl <nguyen_van_phu@allgrow-labo.jp>
2023-03-06 16:32:16 +0700

    update mainvisual

---
+++ assets/data/mockPost.js
@@ -0,0 +1,390 @@
+/* ------------------------------ fakeData Post ----------------------------- */
+
+const mockPost = [
+  {
+    category: "みんなの投稿",
+    typeCategory: "minna",
+    new: true,
+    thumbnail: "./assets/img/post-1.webp",
+    date: "2023.4.13 thu",
+    title: "タイトルが入りますタイトルが入りますタイトルが入ります",
+    author: "万博準備室 名定",
+    avatar: "./assets/img/photo.webp",
+    hashtags: [
+      {
+        link: "#",
+        name: "大阪グルメ",
+      },
+      {
+        link: "#",
+        name: "ミャクミャク",
+      },
+      {
+        link: "#",
+        name: "万博会場探訪",
+      },
+      {
+        link: "#",
+        name: "SDGs",
+      },
+      {
+        link: "#",
+        name: "お得に万博",
+      },
+    ],
+  },
+  {
+    category: "なかま紹介",
+    typeCategory: "nakama",
+    new: true,
+    thumbnail: "./assets/img/post-2.webp",
+    date: "2023.4.13 thu",
+    title: "タイトルが入りますタイトルが入りますタイトルが入ります",
+    author: "万博準備室 名定",
+    avatar: "./assets/img/photo.webp",
+    hashtags: [
+      {
+        link: "#",
+        name: "大阪グルメ",
+      },
+      {
+        link: "#",
+        name: "ミャクミャク",
+      },
+      {
+        link: "#",
+        name: "万博会場探訪",
+      },
+      {
+        link: "#",
+        name: "SDGs",
+      },
+      {
+        link: "#",
+        name: "お得に万博",
+      },
+    ],
+  },
+  {
+    category: "万博SDGs",
+    typeCategory: "sdgs",
+    new: false,
+    thumbnail: "./assets/img/post-3.webp",
+    date: "2023.4.13 thu",
+    title: "タイトルが入りますタイトルが入りますタイトルが入ります",
+    author: "万博準備室 名定",
+    avatar: "./assets/img/photo.webp",
+    hashtags: [
+      {
+        link: "#",
+        name: "大阪グルメ",
+      },
+      {
+        link: "#",
+        name: "ミャクミャク",
+      },
+      {
+        link: "#",
+        name: "万博会場探訪",
+      },
+      {
+        link: "#",
+        name: "SDGs",
+      },
+      {
+        link: "#",
+        name: "お得に万博",
+      },
+    ],
+  },
+  {
+    category: "イベント参加",
+    typeCategory: "event",
+    new: false,
+    thumbnail: "./assets/img/post-4.webp",
+    date: "2023.4.13 thu",
+    title: "タイトルが入りますタイトルが入りますタイトルが入ります",
+    author: "万博準備室 名定",
+    avatar: "./assets/img/photo.webp",
+    hashtags: [
+      {
+        link: "#",
+        name: "大阪グルメ",
+      },
+      {
+        link: "#",
+        name: "ミャクミャク",
+      },
+      {
+        link: "#",
+        name: "万博会場探訪",
+      },
+      {
+        link: "#",
+        name: "SDGs",
+      },
+      {
+        link: "#",
+        name: "お得に万博",
+      },
+    ],
+  },
+  {
+    category: "みんなの投稿",
+    typeCategory: "minna",
+    new: false,
+    thumbnail: "./assets/img/post-5.webp",
+    date: "2023.4.13 thu",
+    title: "タイトルが入りますタイトルが入りますタイトルが入ります",
+    author: "万博準備室 名定",
+    avatar: "./assets/img/photo.webp",
+    hashtags: [
+      {
+        link: "#",
+        name: "大阪グルメ",
+      },
+      {
+        link: "#",
+        name: "ミャクミャク",
+      },
+      {
+        link: "#",
+        name: "万博会場探訪",
+      },
+      {
+        link: "#",
+        name: "SDGs",
+      },
+      {
+        link: "#",
+        name: "お得に万博",
+      },
+    ],
+  },
+  {
+    category: "なかま紹介",
+    typeCategory: "nakama",
+    new: false,
+    thumbnail: "./assets/img/post-6.webp",
+    date: "2023.4.13 thu",
+    title: "タイトルが入りますタイトルが入りますタイトルが入ります",
+    author: "万博準備室 名定",
+    avatar: "./assets/img/photo.webp",
+    hashtags: [
+      {
+        link: "#",
+        name: "大阪グルメ",
+      },
+      {
+        link: "#",
+        name: "ミャクミャク",
+      },
+      {
+        link: "#",
+        name: "万博会場探訪",
+      },
+      {
+        link: "#",
+        name: "SDGs",
+      },
+      {
+        link: "#",
+        name: "お得に万博",
+      },
+    ],
+  },
+  {
+    category: "みんなの投稿",
+    typeCategory: "minna",
+    new: false,
+    thumbnail: "./assets/img/post-1.webp",
+    date: "2023.4.13 thu",
+    title: "タイトルが入りますタイトルが入りますタイトルが入ります",
+    author: "万博準備室 名定",
+    avatar: "./assets/img/photo.webp",
+    hashtags: [
+      {
+        link: "#",
+        name: "大阪グルメ",
+      },
+      {
+        link: "#",
+        name: "ミャクミャク",
+      },
+      {
+        link: "#",
+        name: "万博会場探訪",
+      },
+      {
+        link: "#",
+        name: "SDGs",
+      },
+      {
+        link: "#",
+        name: "お得に万博",
+      },
+    ],
+  },
+  {
+    category: "なかま紹介",
+    typeCategory: "nakama",
+    new: false,
+    thumbnail: "./assets/img/post-2.webp",
+    date: "2023.4.13 thu",
+    title: "タイトルが入りますタイトルが入りますタイトルが入ります",
+    author: "万博準備室 名定",
+    avatar: "./assets/img/photo.webp",
+    hashtags: [
+      {
+        link: "#",
+        name: "大阪グルメ",
+      },
+      {
+        link: "#",
+        name: "ミャクミャク",
+      },
+      {
+        link: "#",
+        name: "万博会場探訪",
+      },
+      {
+        link: "#",
+        name: "SDGs",
+      },
+      {
+        link: "#",
+        name: "お得に万博",
+      },
+    ],
+  },
+  {
+    category: "万博SDGs",
+    typeCategory: "sdgs",
+    new: false,
+    thumbnail: "./assets/img/post-3.webp",
+    date: "2023.4.13 thu",
+    title: "タイトルが入りますタイトルが入りますタイトルが入ります",
+    author: "万博準備室 名定",
+    avatar: "./assets/img/photo.webp",
+    hashtags: [
+      {
+        link: "#",
+        name: "大阪グルメ",
+      },
+      {
+        link: "#",
+        name: "ミャクミャク",
+      },
+      {
+        link: "#",
+        name: "万博会場探訪",
+      },
+      {
+        link: "#",
+        name: "SDGs",
+      },
+      {
+        link: "#",
+        name: "お得に万博",
+      },
+    ],
+  },
+  {
+    category: "イベント参加",
+    typeCategory: "event",
+    new: false,
+    thumbnail: "./assets/img/post-4.webp",
+    date: "2023.4.13 thu",
+    title: "タイトルが入りますタイトルが入りますタイトルが入ります",
+    author: "万博準備室 名定",
+    avatar: "./assets/img/photo.webp",
+    hashtags: [
+      {
+        link: "#",
+        name: "大阪グルメ",
+      },
+      {
+        link: "#",
+        name: "ミャクミャク",
+      },
+      {
+        link: "#",
+        name: "万博会場探訪",
+      },
+      {
+        link: "#",
+        name: "SDGs",
+      },
+      {
+        link: "#",
+        name: "お得に万博",
+      },
+    ],
+  },
+  {
+    category: "みんなの投稿",
+    typeCategory: "minna",
+    new: false,
+    thumbnail: "./assets/img/post-5.webp",
+    date: "2023.4.13 thu",
+    title: "タイトルが入りますタイトルが入りますタイトルが入ります",
+    author: "万博準備室 名定",
+    avatar: "./assets/img/photo.webp",
+    hashtags: [
+      {
+        link: "#",
+        name: "大阪グルメ",
+      },
+      {
+        link: "#",
+        name: "ミャクミャク",
+      },
+      {
+        link: "#",
+        name: "万博会場探訪",
+      },
+      {
+        link: "#",
+        name: "SDGs",
+      },
+      {
+        link: "#",
+        name: "お得に万博",
+      },
+    ],
+  },
+  {
+    category: "なかま紹介",
+    typeCategory: "nakama",
+    new: false,
+    thumbnail: "./assets/img/post-6.webp",
+    date: "2023.4.13 thu",
+    title: "タイトルが入りますタイトルが入りますタイトルが入ります",
+    author: "万博準備室 名定",
+    avatar: "./assets/img/photo.webp",
+    hashtags: [
+      {
+        link: "#",
+        name: "大阪グルメ",
+      },
+      {
+        link: "#",
+        name: "ミャクミャク",
+      },
+      {
+        link: "#",
+        name: "万博会場探訪",
+      },
+      {
+        link: "#",
+        name: "SDGs",
+      },
+      {
+        link: "#",
+        name: "お得に万博",
+      },
+    ],
+  },
+];
+
+export default mockPost;
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,35 @@
+import mockPost from "../data/mockPost.js";
+
 /* --------------------------------- swiper --------------------------------- */
 const swiperMainvisual = new Swiper(".p-mainvisual__swiper", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  speed: 600,
+  speed: 1000,
   allowTouchMove: false,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: true,
+  },
+  fadeEffect: { crossFade: false },
+  virtualTranslate: true,
+  effect: 'fade',
 });
 
 const swiperMovie = new Swiper(".p-movie__swiper", {
-  speed: 600,
+  speed: 1000,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
   autoplay: {
-    delay: 2000,
+    delay: 3000,
+    disableOnInteraction: true,
   },
+  fadeEffect: { crossFade: false },
+  virtualTranslate: true,
+  effect: 'fade',
 });
 
 /* ------------------------------ scroll to top ----------------------------- */
@@ -27,3 +40,56 @@ scrollTop.addEventListener("click", function () {
     behavior: "smooth",
   });
 });
+
+/* ----------------------------- render article ----------------------------- */
+const articleList = document.querySelector("#article-list");
+
+const renderArticle = (data) => {
+  data.forEach((item) => {
+    console.log("item", item);
+
+    let article = `
+        <div class="c-article__items">
+          <a href="#" class="c-article__category ${item.typeCategory}">
+            ${item.category}
+          </a>
+
+          <a href="#" class="c-article__box">
+              <div class="c-article__thumbnail">
+                  <img src="${item.thumbnail}" alt="${item.title}">
+                  
+                  ${item.new ? `<p class="c-article__new">new</p>` : ""}
+              </div>
+
+              <div class="c-article__content">
+                  <p class="c-article__date">
+                    ${item.date}
+                  </p>
+
+                  <h2 class="c-article__title">
+                    ${item.title}
+                  </h2>
+
+                  <figure class="c-article__photo">
+                      <img src="${item.avatar}" alt="${item.author}">
+                      <p class="c-article__name">
+                        ${item.author}
+                      </p>
+                  </figure>
+              </div>
+          </a>
+
+          <div class="c-article__tags">
+            <a href="#">＃大阪グルメ</a>
+            <a href="#">#ミャクミャク</a>
+            <a href="#">＃万博会場探訪</a>
+            <a href="#">＃SDGs</a>
+            <a href="#">＃お得に万博</a>             
+          </div>
+        </div>
+      `;
+    articleList.insertAdjacentHTML("beforeend", article);
+  });
+};
+
+// renderArticle(mockPost);
