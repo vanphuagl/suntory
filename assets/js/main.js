819dfe4e6ca17502b70fa34c762024ca7aab7cdc
vanphuagl <nguyen_van_phu@allgrow-labo.jp>
2023-03-06 08:58:24 +0700

    coding swiper

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
-console.log("javascript");
+/* --------------------------------- swiper --------------------------------- */
+const swiper = new Swiper(".p-mainvisual__swiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  speed: 600,
+  allowTouchMove: false,
+});
+
+/* ------------------------------ scroll to top ----------------------------- */
+const scrollTop = document.querySelector(".c-footer__backtotop");
+scrollTop.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
