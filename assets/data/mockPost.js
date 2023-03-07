01dbd7f577666f399726d14bd3e0d496d98d4b4c
vanphuagl <nguyen_van_phu@allgrow-labo.jp>
2023-03-07 12:08:08 +0700

    update code

--- assets/data/mockPost.js
+++ assets/data/mockPost.js
@@ -1,5 +1,3 @@
-/* ------------------------------ fakeData Post ----------------------------- */
-
 const mockPost = [
   {
     category: "みんなの投稿",
@@ -579,4 +577,82 @@ const mockPost = [
   },
 ];
 
-export default mockPost;
+/* -------------------------------------------------------------------------- */
+/*                                emulator data                               */
+/* -------------------------------------------------------------------------- */
+
+// render article
+const articleList = document.querySelector("#article-list");
+let maxItem = 12;
+
+const renderArticle = (data, max) => {
+  data.slice(0, max).forEach((item) => {
+    // console.log("item", item);
+
+    let article = `
+        <div class="c-article__items">
+          <a href="./listCategory.html" class="c-article__category ${
+            item.typeCategory
+          }">
+            ${item.category}
+          </a>
+
+          <a href="./detail.html" class="c-article__box">
+              <div class="c-article__thumbnail">
+                  <img src="${item.thumbnail}" alt="${item.title}">
+                  
+                  ${item.new ? `<p class="c-article__new">new</p>` : ""}
+              </div>
+
+              <div class="c-article__content">
+                  <p class="c-article__date">
+                    ${item.date}
+                  </p>
+
+                  <h2 class="c-article__title">
+                    ${item.title}
+                  </h2>
+
+                  <figure class="c-article__photo">
+                      <img src="${item.avatar}" alt="${item.author}">
+                      <p class="c-article__name">
+                        ${item.author}
+                      </p>
+                  </figure>
+              </div>
+          </a>
+
+          <div class="c-article__tags">
+            <a href="./listHashtag.html">＃大阪グルメ</a>
+            <a href="./listHashtag.html">#ミャクミャク</a>
+            <a href="./listHashtag.html">＃万博会場探訪</a>
+            <a href="./listHashtag.html">＃SDGs</a>
+            <a href="./listHashtag.html">＃お得に万博</a>             
+          </div>
+        </div>
+      `;
+    articleList.insertAdjacentHTML("beforeend", article);
+  });
+};
+
+renderArticle(mockPost, maxItem);
+
+/* ----------------------------- infinite scroll ---------------------------- */
+// const loading = document.querySelector(".c-loadmore");
+window.addEventListener("scroll", () => {
+  if (loading) {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+    if (clientHeight + scrollTop >= scrollHeight) {
+      // show the loading animation
+      showLoading();
+    }
+  }
+});
+
+function showLoading() {
+  loading.classList.add("show");
+
+  setTimeout(() => {
+    renderArticle(mockPost, 6);
+  }, 500);
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,3 @@
-import mockPost from "../data/mockPost.js";
-
 /* ----------------------------- countdown days ----------------------------- */
 const deadline = new Date("Apr 13, 2025 00:00:00").getTime();
 let getDate = new Date().getTime("ja-JP", {
@@ -116,71 +114,4 @@ window.addEventListener("scroll", () => {
 
 function showLoading() {
   loading.classList.add("show");
-
-  // load more data
-  setTimeout(() => {
-    renderArticle(mockPost, 6);
-  }, 500);
 }
-
-/* -------------------------------------------------------------------------- */
-/*                                emulator data                               */
-/* -------------------------------------------------------------------------- */
-
-// render article
-const articleList = document.querySelector("#article-list");
-let maxItem = 12;
-
-const renderArticle = (data, max) => {
-  if (articleList) {
-    data.slice(0, max).forEach((item) => {
-      // console.log("item", item);
-
-      let article = `
-        <div class="c-article__items">
-          <a href="./listCategory.html" class="c-article__category ${
-            item.typeCategory
-          }">
-            ${item.category}
-          </a>
-
-          <a href="./detail.html" class="c-article__box">
-              <div class="c-article__thumbnail">
-                  <img src="${item.thumbnail}" alt="${item.title}">
-                  
-                  ${item.new ? `<p class="c-article__new">new</p>` : ""}
-              </div>
-
-              <div class="c-article__content">
-                  <p class="c-article__date">
-                    ${item.date}
-                  </p>
-
-                  <h2 class="c-article__title">
-                    ${item.title}
-                  </h2>
-
-                  <figure class="c-article__photo">
-                      <img src="${item.avatar}" alt="${item.author}">
-                      <p class="c-article__name">
-                        ${item.author}
-                      </p>
-                  </figure>
-              </div>
-          </a>
-
-          <div class="c-article__tags">
-            <a href="./listHashtag.html">＃大阪グルメ</a>
-            <a href="./listHashtag.html">#ミャクミャク</a>
-            <a href="./listHashtag.html">＃万博会場探訪</a>
-            <a href="./listHashtag.html">＃SDGs</a>
-            <a href="./listHashtag.html">＃お得に万博</a>             
-          </div>
-        </div>
-      `;
-      articleList.insertAdjacentHTML("beforeend", article);
-    });
-  }
-};
-
-renderArticle(mockPost, maxItem);
